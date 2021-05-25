da09e0e75e8ff83770bae80941a86cdbc244a7cd
General <general-m7@yandex.ru>
2021-05-25 23:55:13 +0300

    init improvement app

--- weather-app/src/components/SelectDate/SelectDate.tsx
+++ weather-app/src/components/SelectDate/SelectDate.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import ContextDate from '../ContextDate';
 import classes from './SelectDate.module.css';
 
